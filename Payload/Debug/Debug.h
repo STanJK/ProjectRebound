95b9e2fd5bc3a69fb1ee70fb75f7be3967f3c0f2
STanJK <117706648+STanJK@users.noreply.github.com>
2026-04-27 18:52:43 +0800

    Debug where?

---
+++ Payload/Debug/Debug.h
@@ -0,0 +1,21 @@
+// Debug.h
+#pragma once
+#include <string>
+#include <fstream>
+#include <mutex>
+
+extern std::mutex LogMutex;
+extern std::string LogFilePath;
+extern bool ClientDebugLogEnabled;
+extern std::ofstream clientLogFile;
+
+std::string CurrentTimestamp();
+void Log(const std::string &msg);
+void ClientLog(const std::string &msg);
+void InitDebugConsole();
+void EnableUnrealConsole();
+void DebugLocateSubsystems();
+void DebugDumpSubsystemsToFile();
+void DebugDumpWeaponPartsToFile();
+void HotkeyThread();
+void ClientAutoDumpThread();
